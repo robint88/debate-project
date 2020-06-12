d779b08569d953d57f079813384b569885265ac1
Robin Turner <robinianturner@gmail.com>
2020-06-12 12:26:24 +0100

    Add unpublished debates to debates page for logged in users

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,19 +1,19 @@
 const navSlide = function(){
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
 // Toggle nav
-    burger.addEventListener('click', function(){
-        nav.classList.toggle("nav-active");
-        // Animate links
-        navLinks.forEach((link, index)=>{
-            if(link.style.animation){
-                link.style.animation = ``;
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 1}s`
-            }
-        });
-        burger.classList.toggle('burgertoggle')
+burger.addEventListener('click', function(){
+    nav.classList.toggle("nav-active");
+    // Animate links
+    navLinks.forEach((link, index)=>{
+        if(link.style.animation){
+            link.style.animation = ``;
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 1}s`
+        }
+    });
+    burger.classList.toggle('burgertoggle')
     });
 }
 
@@ -57,4 +57,6 @@ carouselSlide.addEventListener('transitionend', function(){
         counter = carouselImages.length - counter;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
-})
+});
+
+// UNPUBLISHED DEBATES
--- routes/debates.js
+++ routes/debates.js
@@ -3,7 +3,7 @@ const router = express.Router({mergeParams: true});
 const Debate = require('../models/debate');
 
 router.get("/", function(req, res){
-    Debate.find({isPublished: "true"}).sort({createdAt: 'desc'}).populate("category").exec(function(err, foundDebates){
+    Debate.find({}).sort({createdAt: 'desc'}).populate("category").exec(function(err, foundDebates){
         if(err){
             req.flash('error', 'Something went wrong when finding debates');
             res.redirect('back');
